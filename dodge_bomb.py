1ffbc1dc9f896b4f7a5fdb9c4320d5493d854eb4
Kousei <c0a2316313@edu.teu.ac.jp>
2024-10-01 17:30:54 +0900

    演習2

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,20 +4,19 @@ import sys
 import time
 import pygame as pg
 
-
 WIDTH, HEIGHT = 1100, 650
 DELTA = {
-    pg.K_UP:(0, -5),
-    pg.K_DOWN:(0, +5),
-    pg.K_LEFT:(-5, 0),
-    pg.K_RIGHT:(+5, 0),
-    }
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
+}
 
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
     """
-    引数：こうかとん　または　爆弾のRect
+    引数：こうかとん または 爆弾のRect
     戻り値：真理値タプル（横判定結果、縦判定結果）
     画面内ならTrue、画面外ならFalse
     """
@@ -26,39 +25,51 @@ def check_bound(obj_rct: pg.Rect) -> tuple[bool, bool]:
         yoko = False
     if obj_rct.top < 0 or HEIGHT < obj_rct.bottom:
         tate = False
-    return yoko, tate    
+    return yoko, tate
 
+def create_bomb_images_and_accs() -> tuple[list[pg.Surface], list[int]]:
+    bb_imgs = []
+    bb_accs = [a for a in range(1, 11)]  
+    
+    for r in range(1, 11):
+        bb_img = pg.Surface((20 * r, 20 * r), pg.SRCALPHA)
+        pg.draw.circle(bb_img, (255, 0, 0), (10 * r, 10 * r), 10 * r)
+        bb_imgs.append(bb_img)
+    
+    return bb_imgs, bb_accs
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
-    bg_img = pg.image.load("fig/pg_bg.jpg")    
+    bg_img = pg.image.load("fig/pg_bg.jpg")
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
-    bb_img = pg.Surface((20, 20))
-    bb_img.set_colorkey((0, 0, 0))
-    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)
-    bb_rct = bb_img.get_rect()
+    
+    bb_imgs, bb_accs = create_bomb_images_and_accs()
+    
+    bb_rct = bb_imgs[0].get_rect()
     bb_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    
     vx, vy = +5, -5
     gob_img = pg.Surface((1100, 650))
     gob_img.set_alpha(128)
     pg.draw.rect(gob_img, (0, 0, 0), pg.Rect(0, 0, 800, 1600))
     fonto = pg.font.Font(None, 80)
     txt = fonto.render("GameOver", True, (255, 255, 255))
-    cry_kk_img = pg.image.load("fig\8.png")
-    
-
+    cry_kk_img = pg.image.load("fig/8.png")
 
     clock = pg.time.Clock()
     tmr = 0
+
     while True:
         for event in pg.event.get():
-            if event.type == pg.QUIT: 
+            if event.type == pg.QUIT:
                 return
-        screen.blit(bg_img, [0, 0]) 
-        if kk_rct.colliderect(bb_rct): #工科トンと爆弾が重なっていたら
+
+        screen.blit(bg_img, [0, 0])
+
+        if kk_rct.colliderect(bb_rct):  # 工科トンと爆弾が重なっていたら
             screen.blit(gob_img, (0, 0))
             screen.blit(txt, [420, 280])
             screen.blit(cry_kk_img, (360, 280))
@@ -67,27 +78,24 @@ def main():
             time.sleep(5)
             return
 
-        key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0] #横座標, 縦座標
-        # if key_lst[pg.K_UP]:
-        #     sum_mv[1] -= 5
-        # if key_lst[pg.K_DOWN]:
-        #     sum_mv[1] += 5
-        # if key_lst[pg.K_LEFT]:
-        #     sum_mv[0] -= 5
-        # if key_lst[pg.K_RIGHT]:
-        #     sum_mv[0] += 5
+        
+        idx = min(tmr // 500, 9)  
+        bb_img = bb_imgs[idx]  
+        avx = vx * bb_accs[idx]  
+        avy = vy * bb_accs[idx]
 
+        key_lst = pg.key.get_pressed()
+        sum_mv = [0, 0]
         for key, tpl in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += tpl[0] #横方向
-                sum_mv[1] += tpl[1] #縦方向
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
 
-        bb_rct.move_ip(vx, vy)
+        bb_rct.move_ip(avx, avy)
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
@@ -97,7 +105,6 @@ def main():
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
 
 if __name__ == "__main__":
     pg.init()
