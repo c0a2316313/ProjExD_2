c4410d952d4be05ad7ac8d113fb098ab6f90767b
Kousei <c0a2316313@edu.teu.ac.jp>
2024-10-01 15:17:43 +0900

    練習4

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -43,6 +43,9 @@ def main():
             if event.type == pg.QUIT: 
                 return
         screen.blit(bg_img, [0, 0]) 
+        if kk_rct.colliderect(bb_rct):
+            print("ゲームオーバー")  #工科トンと爆弾が重なっていたら
+            return 
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
